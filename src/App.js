21230e2a967b32e5cf36f01014011b1098036cb5
Riddhi Halani <riddhi@elsner.in>
2023-09-12 11:07:58 +0530

    add changes of first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Footer';
+import Nav from './components/Nav';
+const SignUp = lazy(() => import('./components/SignUp'));
+const PrivateRoute = lazy(() => import('./components/PrivateRoute'));
+const Login = lazy(() => import('./components/Login'));
+const AddProduct = lazy(() => import('./components/AddProduct'));
+const Products = lazy(() => import('./components/Products'));
+const UpdateProduct = lazy(() => import('./components/UpdateProduct'));
+const CreateAlbumForm = lazy(() => import('./components/Albums/CreateAlbum'));
+const AlbumListing = lazy(() => import('./components/Albums/AlbumListing'));
+const UpdateAlbum = lazy(()=> import('./components/Albums/UpdateAlbum'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <PrivateRoute />
+            </Suspense>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Route path='/' element={<Products />}></Route>
+          <Route path='/addproduct' element={<AddProduct />}></Route>
+          <Route path='/logout' element={<h1>Logout</h1>}></Route>
+          <Route path='/update/:id' element={<UpdateProduct />}></Route>
+          <Route path='/createalbum' element={<CreateAlbumForm />}></Route>
+          <Route path="/albumListing" element={<AlbumListing />}></Route>
+          <Route path="/updatealbum/:id" element={<UpdateAlbum />}></Route>
+          <Route path='/profile' element={<h1>Set Your Profile Here</h1>}></Route>
+        </Route>
+        <Route path='/signup' element={<Suspense fallback={<div>Loading...</div>}><SignUp /></Suspense>}></Route>
+        <Route path='/login' element={<Suspense fallback={<div>Loading...</div>}><Login /></Suspense>}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
